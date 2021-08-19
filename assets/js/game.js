85261c2c6ed78c2052e1c15445bcff7b94e36299
RayceWheat <RayceWheat@gmail.com>
2021-08-19 15:52:28 -0500

    Random Health and Attack Values

--- assets/js/game.js
+++ assets/js/game.js
@@ -30,14 +30,17 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney)      
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack -3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
+
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -57,7 +60,10 @@ var fight = function(enemyName) {
     }
 
     // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack -3, enemyAttack);
+    
+    playerHealth = Math.max(0, playerHealth - damage);
+    
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaning."
     );
@@ -88,7 +94,7 @@ for(var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
 
     //reset enemyHleath before starting new fight
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
 
     //use debugger to pause script from running and chek what's going on
     //debugger
@@ -186,6 +192,13 @@ var shop = function() {
       shop();
       break;
   }
+};
+
+//Generates a random number based on min and max input
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  return value;
 };
 
 //Start the game when the page loads
